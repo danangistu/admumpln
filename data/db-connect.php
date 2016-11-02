7c79c51e128cb8dd4c2b7cc79d2b29ae0ea84729
Danang <Danang@webarq.com>
2016-11-02 11:52:25 +0700

    Db Connect

--- data/db-connect.php
+++ data/db-connect.php
@@ -1,6 +1,6 @@
 <?php
 	require_once "rb.php";
-	$conn = R::setup( 'mysql:host=localhost;dbname=admumpln','root', '' );
+	$conn = R::setup( 'mysql:host=219.83.68.91;dbname=danisnuc_admumpln','danisnuc_admin', 'yukihime27' );
 	R::freeze(TRUE);
 	if(!$conn) die("Koneksi gagal");
 	//error_reporting(0);
