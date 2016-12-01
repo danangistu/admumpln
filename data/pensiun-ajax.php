597b6fd24fc899e1109635b506ac4c6b515f3ae1
danang <danang@webarq.com>
2016-12-01 15:38:42 +0700

    Update Data Pensiun

--- data/pensiun-ajax.php
+++ data/pensiun-ajax.php
@@ -2,9 +2,9 @@
 	require_once "db-connect.php";
 	require_once "../function/function.php";
 	try{
-		$rows=R::getAll("	SELECT p.id, p.nip, p.nama, p.jabatan, o.org_unit, p.birthdate,FLOOR(DATEDIFF(CURDATE(),birthdate)/365) as umur ,DATE_ADD(p.birthdate, INTERVAL 55 YEAR) as tgl_pensiun
+		$rows=R::getAll("	SELECT p.id, p.nip, p.nama, p.jabatan, o.org_unit, p.birthdate,FLOOR(DATEDIFF(CURDATE(),birthdate)/365) as umur ,DATE_ADD(p.birthdate, INTERVAL 56 YEAR) as tgl_pensiun
 							FROM pegawai p, orgunit o
-							WHERE p.id_org_unit=o.id_org_unit AND (FLOOR(DATEDIFF(CURDATE(),birthdate)/365)='54' OR FLOOR(DATEDIFF(CURDATE(),birthdate)/365)='55')
+							WHERE p.id_org_unit=o.id_org_unit AND (FLOOR(DATEDIFF(CURDATE(),birthdate)/365)='54' OR FLOOR(DATEDIFF(CURDATE(),birthdate)/365)='55' OR FLOOR(DATEDIFF(CURDATE(),birthdate)/365)='56')
 							ORDER BY p.birthdate ASC
 						");
 		foreach($rows as $row){
@@ -15,7 +15,7 @@
 			array_push($col,$row['org_unit']);
 			array_push($col,$row['birthdate']);
 			array_push($col,$row['tgl_pensiun']);
-			if($row['umur']<=54)
+			if($row['umur']<=55)
 				array_push($col,"Aktif");
 			else
 				array_push($col,"Sudah Pensiun");
